d3f0657d45fdc2448d9b56c212515fffa1b058e8
janicecaldeira <janicecaldeira@gmail.com>
2021-10-13 23:00:09 -0300

    Atualizando Projeto

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,5 +1,10 @@
+import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty({ message: 'Informe um endereço de e-mail' })
+  @IsEmail({}, { message: 'Informe um e-mail válido' })
   email: string;
+
   name: string;
   password: string;
   passwordConfirmation: string;
