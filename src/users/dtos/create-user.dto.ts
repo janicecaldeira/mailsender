7869ef37ee8ca493ab14b642dc4a9ad71af01c46
janicecaldeira <janicecaldeira@gmail.com>
2021-10-14 19:26:38 -0300

    Inserindo validações

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,13 +1,32 @@
-import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
+import {
+  IsString,
+  IsEmail,
+  IsNotEmpty,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
+  @IsString()
   @IsNotEmpty({ message: 'Informe um endereço de e-mail' })
   @IsEmail({}, { message: 'Informe um e-mail válido' })
+  @MaxLength(200, { message: 'O e-mail deve ter menos de 200 caracteres' })
   email: string;
 
+  @IsString()
+  @IsNotEmpty({ message: 'Informe um nome de usuário' })
+  @MaxLength(200, { message: 'O nome deve ter menos de 200 caracteres' })
   name: string;
 
+  @IsString()
+  @IsNotEmpty({ message: 'Informe uma senha' })
+  @MinLength(6, { message: 'A senha deve ter pelo menos 6 caracteres' })
   password: string;
 
+  @IsString()
+  @IsNotEmpty({ message: 'Informe a confirmação da senha' })
+  @MinLength(6, {
+    message: 'A confirmação da senha deve ter pelo menos 6 caracteres',
+  })
   passwordConfirmation: string;
 }
